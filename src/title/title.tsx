6ff2b634181b56f957ae42400642d16290ba4471
Gargi2103 <86493986+Gargi2103@users.noreply.github.com>
2021-06-26 03:48:24 +0530

    Adding Title Component Using React and Typescript

---
+++ src/title/title.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+
+interface TitleProps {
+    title: string;
+    subtitle: string;
+}
+
+const Title : FC<TitleProps> = ({title, subtitle}) => {
+    return (
+        <div>
+            <h1>
+                {title}
+            </h1>
+            <h2>
+                {subtitle}
+            </h2>
+        </div>
+    );
+};
+
+export default Title;
